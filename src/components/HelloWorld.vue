64ceb0fdcab93644e4bb640e61d1e0b3fca004cd
ArtemY <artemisko12345@gmail.com>
2024-02-06 16:09:33 +0700

    ffgege

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       For a guide and reciasdasdgit pes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentaccccction</a
+        >vue-cli documentwfwefwefwgit adaccccction</a
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
